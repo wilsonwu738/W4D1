eff43568ab72f9e828eacdf079956a1c8f78f0f0
Wilson Wu <wuyecheng738@gmail.com>
2022-11-28 15:23:12 -0500

    finished phase 3

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -7,5 +7,23 @@ class PolyTreeNode
         @children = children
     end
 
-    def parent=()
+    def parent=(node)        #A(B)
+        return @parent = nil if node == nil
+        @parent.children.delete(self) unless @parent == nil
+        @parent = node    #setting B as A's parent 
+        if !node.children.include?(self) 
+            node.children << self    #set A as B's child
+        end
+    end
+
+    def add_child(child_node)
+        child_node.parent = self
+    end
+
+    def remove_child(child_node)
+        raise "error" if child_node.parent == nil
+        child_node.parent = nil
+    end
+
+
 end
